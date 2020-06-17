09533551ad096ce4ff742661fbd0b866d495a781
Joselucaas <lucas.chag0608@gmail.com>
2020-06-17 09:22:30 -0300

    Update aposentadoria.js

--- Desafio01/aposentadoria.js
+++ Desafio01/aposentadoria.js
@@ -1,8 +1,9 @@
 const nome = "Carla"
 const sexo = "F"
-const idade = 26
-const contribuicao = 66
+const idade = 55
+const contribuicao = 37
 
+const regraSoma = idade + contribuicao
 // O tempo de contribuição mínimo para homens é de 35 anos e
 // para mulheres é de 30 anos
 
@@ -10,7 +11,7 @@ const contribuicao = 66
 // enquanto a mulher precisa de no mínimo 85 anos
 
 if (sexo === "M") {
-    if (idade + contribuicao >= 95) {
+    if (regraSoma >= 95 && contribuicao >= 35) {
         console.log(`${nome}, você pode se aposentar`)
     } else {
         console.log(`${nome}, você ainda não pode se aposentar`)
@@ -18,9 +19,9 @@ if (sexo === "M") {
 }
 
 if (sexo === "F") {
-    if(idade + contribuicao >= 85) {
+    if(regraSoma >= 85 && contribuicao >= 30) {
         console.log(`${nome}, você pode se aposentar`)
     } else {
         console.log(`${nome}, você ainda não pode se aposentar`)
     }
-}
+} 
