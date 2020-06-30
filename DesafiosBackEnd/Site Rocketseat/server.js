36922d9518bd6a883596e109bac2f49c3c5e730a
Joselucaas <lucas.chag0608@gmail.com>
2020-06-30 03:11:25 -0300

    Back End

---
+++ DesafiosBackEnd/Site Rocketseat/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+// Pegando o CSS
+server.use(express.static("publics"))
+
+// Pegando HTML
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+ 
+// Trazando o front-end do site para o servidor
+server.get('/', function(req, res) {
+    return res.render("about")
+})
+
+server.get('/courses', function(req, res) {
+    return res.render("courses")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+// ----------------------------------------
+
+// Trazando o front-end do site para o servidor
+server.listen(2001, function() {
+    console.log("Server is running")
+})
