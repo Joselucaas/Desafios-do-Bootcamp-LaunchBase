30cc62ce9296948c66ca0d209fbaadc6286dbe0c
Joselucaas <lucas.chag0608@gmail.com>
2020-07-02 01:07:26 -0300

    Back end

---
+++ DesafiosBackEnd/data.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: "starter",
+        circle: "circle",
+        number: "01",
+        image: "/img/starter.svg",
+        description: "<spam>Torne-se um programador desejado</spam> no mercado com esses cursos gratuitos.",
+        modules: "5 módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        circle: "circle-lb",
+        id_main: "lb",
+        number: "02",
+        image: "/img/launchbase.svg",
+        description: "<spam>Domine programação do zero</spam> e tenha acesso às melhores oportunidades do mercado.",
+        modules: "16 módulos",
+        price: "Pago",
+    },
+    {
+        id: "gostack",
+        circle: "circle-go",
+        id_main: "go",
+        number: "03",
+        image: "/img/gostack.svg",
+        description: "<spam>Treinamento imersivo</spam> nas tecnologias mais modernas de desenvolvimento web e mobile.",
+        modules: "27 módulos",
+        price: "Pago",
+    },
+]
--- DesafiosBackEnd/server.js
+++ DesafiosBackEnd/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const dados = require('./data') // Pegando o script data.js
 
 // Pegando o CSS
 server.use(express.static("publics"))
@@ -10,16 +11,39 @@ server.use(express.static("publics"))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
  
 // Trazando o front-end do site para o servidor
 server.get('/', function(req, res) {
-    return res.render("about")
+    const date = {
+        title: "Rocketseat",
+        image: "/img/rckt.jpg",
+        description: "Nós somos a Rocketseat, uma empresa localizada no sul do Brasil, em Santa Catarina. Voltada para o ensino de tecnologias para Dev's de qualquer nível. Mas além de ensinar, nós o preparamos para o mercado de trabalho, com todos os desafios e tecnologias do atual mercado através dos bootcamp's GoStack e LaunchBase. Abaixo algumas das tecnologias que ensinamos em nossos Bootcamp's.",
+        techs: [
+            {name: "JavaScript"},
+            {name: "-"},
+            {name: "HTML"},
+            {name: "-"},
+            {name: "CSS"},
+            {name: "-"},
+            {name: "NodeJs"},
+            {name: "-"},
+            {name: "React Native"}
+        ],
+        network: [
+            {name: "GitHub", link: "https://github.com/Rocketseat"},
+            {name: "Instagram", link: "https://www.instagram.com/rocketseat_oficial/"},
+            {name: "Facebook", link: "https://www.facebook.com/rocketseat"}
+        ]
+    }
+
+    return res.render("about", {date})
 })
 
 server.get('/courses', function(req, res) {
-    return res.render("courses")
+    return res.render("courses", { items: dados}) // Passsando o data.js do back para o front-end
 })
 
 server.use(function(req, res) {
